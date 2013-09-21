fbbdd37c8cf882d28a160793224f1900b3d4763f
Ken "Minalien" Murray <minalien@gmail.com>
2013-09-21 16:32:52 -0700

    Updated NSSEditor, added preview window overrides. Editor and preview functionality will be implemented after more research on custom editor controls.
    Signed-off-by: Ken "Minalien" Murray <minalien@gmail.com>

--- Assets/Scripts/Editor/NodeSkeletonSystem/NSSEditor.cs
+++ Assets/Scripts/Editor/NodeSkeletonSystem/NSSEditor.cs
@@ -27,9 +27,29 @@ namespace Editor.NodeSkeletonSystem
 		/** END MENU ITEMS **/
 		
 		/// <summary>
-		/// Implements the custom editor's Inspector window GUI.
+		/// Implements the GUI displayed in the inspector, so that items can be entered by value.
 		/// </summary>
 		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+			
+			// Set the target as dirty if the GUI values have changed.
+			if(GUI.changed)
+				EditorUtility.SetDirty(Target);
+		}
+		
+		/// <summary>
+		/// Mark this editor has having a preview GUI.
+		/// </summary>
+		public override bool HasPreviewGUI()
+		{
+			return true;
+		}
+		
+		/// <summary>
+		/// Renders a quad, potentially with a preview texture, with the individual points and names representing the nodes.
+		/// </summary>
+		public void OnPreviewGUI()
 		{
 		}
 	}
