1c4ad051d083641d16d4adcb9276010730aff1ca
Ken 'Minalien' Murray <minalien@gmail.com>
2013-09-20 23:20:36 -0700

    AnimatedSpriteBehavior implementation is complete. Utilizes a single-level animation sequence (see Assets/Textures/Animation Test/Running.png) - Note that this texture MUST BE REMOVED and not distributed with this project - it is here only so that the animation system can be tested. Once we have art assets ready, we can use one of those as the test/sample image.
    Signed-off-by: Ken 'Minalien' Murray <minalien@gmail.com>

---
+++ Assets/Scripts/Behaviors/Sprites/AnimatedSpriteBehavior.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Implements animated sprite functionality, to be applied to a Quad.
+/// 
+/// Author: Ken Murray
+/// </summary>
+[ExecuteInEditMode]
+public class AnimatedSpriteBehavior : MonoBehaviour
+{
+	/// <summary>
+	/// Speed, in frames per second, at which the animation will play.
+	/// </summary>
+	public float AnimationSpeed = 10.0f;
+
+	/// <summary>
+	/// Texture sheet containing the animations.
+	/// </summary>
+	public Texture2D AnimationSheet;
+
+	/// <summary>
+	/// Number of frames contained in the image.
+	/// </summary>
+	public int FrameCount;
+
+	/// <summary>
+	/// Stores a list of offsets for each frame.
+	/// </summary>
+	private Vector2[] frameOffsets;
+
+	/// <summary>
+	/// Current animation frame being displayed.
+	/// </summary>
+	private int currentFrame;
+
+	/// <summary>
+	/// Amount of time that has passed since the last frame.
+	/// </summary>
+	private float frameDelta;
+
+	/// <summary>
+	/// Size of each frame.
+	/// </summary>
+	private Vector2 frameSize;
+
+	/// <summary>
+	/// Calculates the total number of frames in the image, based on the width and height of the texture and frames.
+	/// </summary>
+	public void Start()
+	{
+		if (AnimationSheet == null)
+			return;
+
+		if (FrameCount <= 0)
+			FrameCount = 1;
+
+		frameOffsets = new Vector2[FrameCount];
+
+		float frameWidth = 1.0f / FrameCount;
+
+		for (int _i = 0; _i < FrameCount; _i++)
+			frameOffsets[_i] = new Vector2(_i * frameWidth, 0.0f);
+
+		frameSize = new Vector2(1.0f / FrameCount, 1.0f);
+
+		gameObject.renderer.sharedMaterial.SetTextureScale("_MainTex", frameSize);
+	}
+
+	/// <summary>
+	/// Increments the current frame if the frame time has been passed.
+	/// </summary>
+	public void Update()
+	{
+		frameDelta += Time.deltaTime;
+
+		if (frameDelta >= (1.0f / AnimationSpeed)) {
+			frameDelta -= (1.0f / AnimationSpeed);
+
+			currentFrame++;
+
+			if (currentFrame >= FrameCount)
+				currentFrame = 0;
+
+			gameObject.renderer.sharedMaterial.SetTextureOffset("_MainTex", frameOffsets[currentFrame]);
+		}
+	}
+}
