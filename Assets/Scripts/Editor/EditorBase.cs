6cff4a5b1f79651d1acb7b08cc95d9ffa688b036
Ken "Minalien" Murray <minalien@gmail.com>
2013-09-21 15:19:47 -0700

    Added EditorBase as a base class for inspector-based custom editor extensions. Began work on adding Node Skeleton Structure (NSS) editor.
    Signed-off-by: Ken "Minalien" Murray <minalien@gmail.com>

---
+++ Assets/Scripts/Editor/EditorBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UEd = UnityEditor.Editor;
+
+namespace Editor
+{
+	/// <summary>
+	/// Serves as a base class for all Inspector-based editors, to work around
+	/// <see cref="Editor" />'s use of a generic <see cref="UnityEngine.Object">.
+	/// 
+	/// Based on code from:
+	/// http://www.gamasutra.com/view/news/126740/Opinion_Extending_The_Unity3D_Editor.php
+	/// </summary>
+	/// <example>
+	/// [CustomEditor(typeof(TestBehavior))]
+	/// public class MyEditor : EditorBase<TestBehavior> {
+	/// }
+	/// </example>
+	public class EditorBase<T> : UEd where T : UnityEngine.Object
+	{
+		/// <summary>
+		/// Provides a strongly-typed reference to the editor target.
+		/// </summary>
+		protected T Target { get { return (T)target; } }
+	}
+}
---
+++ Assets/Scripts/Editor/NodeSkeletonSystem/NSSEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Editor.NodeSkeletonSystem
+{
+	/// <summary>
+	/// Unity editor window that allows for the creation and modification of Node Skeleton Structure (NSS) files.
+	/// 
+	/// Author: Ken Murray
+	/// </summary>
+	public class NSSEditor : EditorWindow
+	{
+		/// <summary>
+		/// Menu item (Tactibru -> Node Skeleton System -> NSS Editor) that the NSS Editor window.
+		/// </summary>
+		[MenuItem("Tactibru/Node Skeleton System/NSS Editor")]
+		public static void ShowWindow()
+		{
+			EditorWindow.GetWindow (typeof(NSSEditor));
+		}
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Editor.NodeSkeletonSystem.NSSEditor"/> class.
+		/// </summary>
+		public NSSEditor()
+		{
+			title = "NSS Editor";
+		}
+	}
+}
