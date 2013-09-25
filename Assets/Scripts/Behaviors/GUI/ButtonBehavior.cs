96f16408e193a7f851ec4581f8b5c22195506e56
kjmatthews10054 <karmatth@uat.edu>
2013-09-24 19:34:13 -0700

    Added ButtonControllerBehavior.cs that constructs a ray for button functionality for ButtonBehavior.cs. Deleted my old ButtonControllerScript.cs and ButtonControllerBehavior.cs

---
+++ Assets/Scripts/Behaviors/GUI/ButtonBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Script that handles the functionality of my Button Controller, such as Mouse Up, Mouse Down, and Mouse Over
+/// 
+/// Author: Karl John Matthews
+/// </summary>
+public class ButtonBehavior : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+       
+	}
+    /// <summary>
+    /// Function that prints a statement saying that my current mouse position is over the cube
+    /// </summary>
+    public void MouseOver()
+    {
+        Debug.Log("Mouse is over Cube");
+    }
+}
---
+++ Assets/Scripts/Behaviors/GUI/ButtonControllerBehavior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Script that constructs a ray from the current mouse coordinates. Then calls a MouseOver function if the ray hits an object.
+/// 
+/// Author: Karl John Matthews
+/// </summary>
+public class ButtonControllerBehavior : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    /// <summary>
+    /// Function that constructs a ray from the current mouse position.
+    /// Checks to see if the ray hits an object, if it does then create a ButtonBehavior object and get the ButtonBehavior component
+    /// If the object has been created then call MouseOver function
+    /// </summary>
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            // Create a new ButtonBehavior in Scripts/Behaviors/GUI
+            // Instead of checking for ButtonControllerBehavior, check for ButtonBehavior
+            // This scripts goes on either Camera or other empty object
+            // ButtonBehavior goes on button object!
+            ButtonBehavior buttonControl = hitInfo.collider.GetComponent<ButtonBehavior>();
+            if (buttonControl != null)
+                buttonControl.MouseOver();
+  
+        }
+    }
+
+}
