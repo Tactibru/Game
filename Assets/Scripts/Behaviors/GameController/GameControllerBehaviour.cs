803478fc02d13867d5f98f5093f9b56ecd1595a2
Alex Reiss <ghostbearr@gmail.com>
2013-10-08 11:49:08 -0700

    Added the game game controller not done yet, will commit later.

---
+++ Assets/Scripts/Behaviors/GameController/GameControllerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameControllerBehaviour : MonoBehaviour 
+{
+    public List<ActorBehavior> playerTeam = new List<ActorBehavior>();
+    public List<ActorBehavior> enemyTeam = new List<ActorBehavior>();
+    public List<ActorBehavior> nuetrals = new List<ActorBehavior>();
+
+	// Use this for initialization
+	void Start () 
+    {
+        for (int index = 0; index < playerTeam.Count; index++)
+            playerTeam[index].side = 0;
+
+        for (int index = 0; index < enemyTeam.Count; index++)
+            enemyTeam[index].side = 1;
+
+        for (int index = 0; index < nuetrals.Count; index++)
+            nuetrals[index].side = 2;
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+}
