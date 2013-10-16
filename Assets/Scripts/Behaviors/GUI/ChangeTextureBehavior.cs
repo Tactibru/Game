6ec5a67e3965fd4c2a11fb884c119db382f1764f
kjmatthews10054 <karmatth@uat.edu>
2013-10-15 23:04:05 -0700

    Changed the MainMenuGUITest.unity to use my custom buttons instead of OnGUI. Now the Main Menu buttons change color based off the button state. Still need to be able to change the scene and add doc comments.
    Signed-off-by: kjmatthews10054 <karmatth@uat.edu>

--- Assets/Scripts/Behaviors/GUI/ChangeTextureBehavior.cs
+++ Assets/Scripts/Behaviors/GUI/ChangeTextureBehavior.cs
@@ -2,57 +2,56 @@
 using System.Collections;
 /// <summary>
 /// Behavior handles changing the texture of the quads depending on whether or not the mouse is hovering over the object, clicking on the object or not clicking on the object.
-/// 
+///
 /// Author: Karl Matthews
 /// </summary>
-public class ChangeTextureBehavior : MonoBehaviour 
+public class ChangeTextureBehavior : MonoBehaviour
 {
-	/// <summary>
-	/// Public variables representing the three different textures that will appear on the quads depending on the mouse input.
-	/// A public boolean variable that is true if the left mouse button has been pressed and false if it has been released. 
-	/// </summary>
-	public Texture2D normalTexture;
-	public Texture2D pressedTexture;
-	public Texture2D hoverTexture;
-	public static bool pressed = false;
-	
-	/// <summary>
-	/// Function that sets the texture to my hovering over texture if the the button has not been pressed and the mouse is hovering over the object.
-	/// </summary>
-	void OnMouseEnter()
-	{
-		if(!pressed)
-		{
-			renderer.material.SetTexture("_MainTex", hoverTexture);
-		}
-	}
-	/// <summary>
-	/// If the left mouse button has been pressed then change the texture of the quad to the pressed texture and set my pressed boolean to true.
-	/// </summary>
-	void OnMouseDown()
-	{
-		pressed = true;
-		renderer.material.SetTexture("_MainTex", pressedTexture);
-	}
-	/// <summary>
-	/// Once the mouse is not hovering over the object and the left mouse button is not being pressed then set the texture of the quad to my normal texture.
-	/// </summary>
-	void OnMouseExit()
-	{
-		if(!pressed)
-		{
-			renderer.material.SetTexture("_MainTex", normalTexture);
-		}
-	}
-	/// <summary>
-	/// If the left mouse button has been released then set the quad texture to my normal texture and turn pressed to false.
-	/// </summary>
-	void OnMouseUp()
-	{
-		renderer.material.SetTexture("_MainTex", normalTexture);
-		pressed = false;
-	}
-	
-	
-	
+        /// <summary>
+        /// Public variables representing the three different textures that will appear on the quads depending on the mouse input.
+        /// A public boolean variable that is true if the left mouse button has been pressed and false if it has been released.
+        /// </summary>
+        public TextMesh text;
+        public static bool pressed = false;
+       
+ 
+        /// <summary>
+        /// Function that sets the texture to my hovering over texture if the the button has not been pressed and the mouse is hovering over the object.
+        /// </summary>
+        void OnMouseEnter()
+        {
+                if(!pressed)
+                {
+                        text.color = Color.yellow;
+                }
+        }
+        /// <summary>
+        /// If the left mouse button has been pressed then change the texture of the quad to the pressed texture and set my pressed boolean to true.
+        /// </summary>
+        void OnMouseDown()
+        {
+                text.color = Color.green;
+                pressed = true;
+        }
+        /// <summary>
+        /// Once the mouse is not hovering over the object and the left mouse button is not being pressed then set the texture of the quad to my normal texture.
+        /// </summary>
+        void OnMouseExit()
+        {
+                if(!pressed)
+                {
+                        text.color = Color.red;
+                }
+        }
+        /// <summary>
+        /// If the left mouse button has been released then set the quad texture to my normal texture and turn pressed to false.
+        /// </summary>
+        void OnMouseUp()
+        {
+                text.color = Color.red;
+                pressed = false;
+        }
+       
+       
+       
 }
