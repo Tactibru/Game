c0b5ae0bfd41e15bae59d0e5de68799cf3db8d2e
Adester <Dsternejr@gmail.com>
2013-10-21 13:14:06 -0700

    I tried several different ways to load or save the data but there isn't any list of squads or list of enemies to pull data. I might try finding all gameobjects and finding their coords and trying to pull their data from there.

---
+++ Assets/Scripts/Behaviors/LoadSaveBehavior.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
+
+public class LoadSaveBehavior : MonoBehaviour
+{
+    private Rect save;
+    private Rect load;
+    private Rect saveMSG;
+    private Rect loadMSG;
+    private bool shouldSave;
+    private bool shouldLoad;
+    private bool switchSave;
+    private bool switchLoad;
+    private string fileLocation;
+    private string fileName;
+    private string playerName;
+    private string data;
+    private UserData myData;
+    private Vector3 VPosition;
+    public GameObject Player;
+
+    void Start()
+    {
+        save = new Rect(10, 80, 100, 20);
+        load = new Rect(10, 100, 100, 20);
+        saveMSG = new Rect(10, 120, 400, 40);
+        loadMSG = new Rect(10, 140, 400, 40);
+
+        // Where we want to save and load to and from 
+        fileLocation = Application.dataPath;
+        fileName = "SaveData.xml";
+
+        // we need soemthing to store the information into 
+        myData = new UserData();
+    }
+
+    void Update() { }
+
+    void OnGUI()
+    {
+
+        //*************************************************** 
+        // Loading The Player... 
+        // **************************************************       
+        if (GUI.Button(load, "Load"))
+        {
+
+            GUI.Label(loadMSG, "Loading from: " + fileLocation);
+            // Load our UserData into myData 
+            LoadXML();
+            if (data.ToString() != "")
+            {
+                // notice how I use a reference to type (UserData) here, you need this 
+                // so that the returned object is converted into the correct type 
+                myData = (UserData)DeserializeObject(data);
+                // set the players position to the data we loaded 
+                VPosition = new Vector3(myData._iUser.x, myData._iUser.y, myData._iUser.z);
+                Player.transform.position = VPosition;
+                // just a way to show that we loaded in ok 
+                Debug.Log(myData._iUser.name);
+            }
+
+        }
+
+        //*************************************************** 
+        // Saving The Player... 
+        // **************************************************    
+        if (GUI.Button(save, "Save"))
+        {
+
+            GUI.Label(saveMSG, "Saving to: " + fileLocation);
+            myData._iUser.x = Player.transform.position.x;
+            myData._iUser.y = Player.transform.position.y;
+            myData._iUser.z = Player.transform.position.z;
+            myData._iUser.name = playerName;
+
+            // Time to creat our XML! 
+            data = SerializeObject(myData);
+            // This is the final resulting XML from the serialization process 
+            CreateXML();
+            Debug.Log(data);
+        }
+
+
+    }
+
+    /* The following metods came from the referenced URL */
+    string UTF8ByteArrayToString(byte[] characters)
+    {
+        UTF8Encoding encoding = new UTF8Encoding();
+        string constructedString = encoding.GetString(characters);
+        return (constructedString);
+    }
+
+    byte[] StringToUTF8ByteArray(string pXmlString)
+    {
+        UTF8Encoding encoding = new UTF8Encoding();
+        byte[] byteArray = encoding.GetBytes(pXmlString);
+        return byteArray;
+    }
+
+    // Here we serialize our UserData object of myData 
+    string SerializeObject(object pObject)
+    {
+        string XmlizedString = null;
+        MemoryStream memoryStream = new MemoryStream();
+        XmlSerializer xs = new XmlSerializer(typeof(UserData));
+        XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+        xs.Serialize(xmlTextWriter, pObject);
+        memoryStream = (MemoryStream)xmlTextWriter.BaseStream;
+        XmlizedString = UTF8ByteArrayToString(memoryStream.ToArray());
+        return XmlizedString;
+    }
+
+    // Here we deserialize it back into its original form 
+    object DeserializeObject(string pXmlizedString)
+    {
+        XmlSerializer xs = new XmlSerializer(typeof(UserData));
+        MemoryStream memoryStream = new MemoryStream(StringToUTF8ByteArray(pXmlizedString));
+        XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+        return xs.Deserialize(memoryStream);
+    }
+
+    // Finally our save and load methods for the file itself 
+    void CreateXML()
+    {
+        StreamWriter writer;
+        FileInfo t = new FileInfo(fileLocation + "\\" + fileName);
+        if (!t.Exists)
+        {
+            writer = t.CreateText();
+        }
+        else
+        {
+            t.Delete();
+            writer = t.CreateText();
+        }
+        writer.Write(data);
+        writer.Close();
+        Debug.Log("File written.");
+    }
+
+    void LoadXML()
+    {
+        StreamReader r = File.OpenText(fileLocation + "\\" + fileName);
+        string _info = r.ReadToEnd();
+        r.Close();
+        data = _info;
+        Debug.Log("File Read");
+    }
+}
+
+public class UserData
+{
+    // We have to define a default instance of the structure 
+    public UserData iUser;
+    // Default constructor doesn't really do anything at the moment 
+    public UserData() { }
+
+    //For now saves player coords and name
+    //need to add in squads etc.
+    public struct UserData
+    {
+        public float x;
+        public float y;
+        public float z;
+        public string name;
+    }
+}
