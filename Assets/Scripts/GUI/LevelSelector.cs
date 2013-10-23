d4e87f5f68b7b383f972ceb93d98bc9481f8aeb2
Hobo42 <taiteryan@gmail.com>
2013-10-23 16:41:51 -0700

    Made progress on finding a work around to loading the levels in Game Levels into an array. Currently it is able to print out each file in the folder, next is making sure we don't catch the unity.meta files in the process.
    Signed-off-by: Hobo42 <taiteryan@gmail.com>

--- Assets/Scripts/GUI/LevelSelector.cs
+++ Assets/Scripts/GUI/LevelSelector.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 
 /// <summary>
@@ -11,6 +12,35 @@ using System.Collections;
 public class LevelSelector : ButtonManagerBehavior
 {
 	/// <summary>
+	/// The Application.dataPath stored for easier use
+	/// </summary>
+	public static string assetPath = Application.dataPath;
+	/// <summary>
+	/// The Game Levels directory path.
+	/// </summary>
+	public static string gameLevelsPath = assetPath + "\\Scenes\\Game Levels\\";
+	/// <summary>
+	/// Number of levels in the Game Levels folder
+	/// </summary>
+	public static int levelCount = 0;
+ 
+	/// <summary>
+	/// Array that holds the levels from GameLevels
+	/// Currently a work in progress. 
+	/// Set to a size of 20, but only until I know how many levels are actually in the Game Levels folder.
+	/// </summary>
+	public GameScene[] levelArray = new GameScene[levelCount];
+	
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	public void Start()
+	{
+		CheckIfDirectoryExists_TEST();
+		scanDirectoryForFiles(gameLevelsPath);
+	}
+	
+	/// <summary>
 	/// Activate the button that will be pressed.
 	/// </summary>
 	/// <param name='buttonName'>
@@ -18,6 +48,8 @@ public class LevelSelector : ButtonManagerBehavior
 	/// </param>
 	public override void ButtonPressed (string buttonName)
 	{
+		
+		
 		/// Switch statement that loads the approiate level based on the level name passed in.
 		switch(buttonName)
 		{
@@ -42,6 +74,49 @@ public class LevelSelector : ButtonManagerBehavior
 			base.ButtonPressed(buttonName);
 			break;
 		}
-
+	}
+	
+	/// <summary>
+	/// Checks if directory and certain files exist in the project.
+	/// This is for testing purposes.
+	/// </summary>
+	public void CheckIfDirectoryExists_TEST()
+	{
+		/// Check if the Game Levels dir exists in the Assest path
+		if(Directory.Exists(gameLevelsPath))
+		{
+			//Debug.Log(assetPath + "\\Scenes\\Game Levels\\");
+			Debug.Log("Game Levels Dir exists!");
+		}
+		else
+		{
+			Debug.Log("Game Levels Dir does NOT exist!");
+		}
+		
+		/// Check if the levels in Game Levels exist
+		if(File.Exists(gameLevelsPath + "level03.unity"))
+		{
+			Debug.Log("level03.unity does exist!");
+		}
+		else
+		{
+			Debug.Log("level03.unity does NOT exist!");
+		}
+	}
+	
+	/// <summary>
+	/// Scans the directory for files.
+	/// </summary>
+	/// <param name='dirPath'>
+	/// Directory path we want to scan for files
+	/// </param>
+	private void scanDirectoryForFiles(string dirPath)
+	{
+		string[] fileArray = Directory.GetFiles(dirPath);
+		
+		foreach(string fileName in fileArray)
+		{
+			Debug.Log(fileName);
+		}
 	}
 }
