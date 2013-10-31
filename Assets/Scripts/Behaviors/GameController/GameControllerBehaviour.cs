6cd4f9ea0b350e6477d719818a3f42a1c4882ff6
Alex Reiss <ghostbearr@gmail.com>
2013-10-30 21:35:22 -0700

    The end game is refacted.

--- Assets/Scripts/Behaviors/GameController/GameControllerBehaviour.cs
+++ Assets/Scripts/Behaviors/GameController/GameControllerBehaviour.cs
@@ -57,15 +57,16 @@ public class GameControllerBehaviour : MonoBehaviour
     /// </summary>
     void Update()
     {
-        if (enemyTeamTotal == 0)
-        {
-            Application.LoadLevel("PlayerWins");
-        }
+        EndGame();
+        //if (enemyTeamTotal == 0)
+        //{
+        //    Application.LoadLevel("PlayerWins");
+        //}
 
-        if (playerTeamTotal == 0)
-        {
-            Application.LoadLevel("PlayerLosses");
-        }
+        //if (playerTeamTotal == 0)
+        //{
+        //    Application.LoadLevel("PlayerLosses");
+        //}
 
         if (Input.GetKeyDown(KeyCode.Space) || leftToMoveThis == 0)
         {
@@ -90,6 +91,19 @@ public class GameControllerBehaviour : MonoBehaviour
     //            currentTurn = UnitSide.player;
     //            leftToMoveThis = playerTeamTotal;
     //        }
+        }
+    }
+
+    public void EndGame()
+    {
+        if (enemyTeamTotal == 0)
+        {
+            Application.LoadLevel("PlayerWins");
+        }
+
+        if (playerTeamTotal == 0)
+        {
+            Application.LoadLevel("PlayerLosses");
         }
     }
 
