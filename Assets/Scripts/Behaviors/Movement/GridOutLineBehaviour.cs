84708750f5e2de5ed8228075466b4e396744851c
Alex Reiss <ghostbearr@gmail.com>
2013-11-01 15:57:52 -0700

    Built the unit creator test scene, no coding work has been done yet.

---
+++ Assets/Scripts/Behaviors/Movement/GridOutLineBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridOutLineBehaviour : MonoBehaviour
+{
+
+    public GridBehavior theGrid;
+    public LineRenderer theLine;
+
+    void Start()
+    {
+        theGrid = GameObject.FindGameObjectWithTag("Grid").GetComponent<GridBehavior>();
+        theLine.SetWidth(0.1f, 0.1f);
+        //theLine.SetVertexCount();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (MovePointBehavior movePoint in theGrid.theMap)
+        {
+ 
+        }
+    }
+}
