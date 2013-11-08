4e9ee3ac07ca351d5488eadcc756823034566865
adester <Dsternejr@gmail.com>
2013-11-07 19:09:24 -0700

    Merged Master Repository into mine to fix some errors. Working on the Minimap HUD more tomorrow.
    Signed-off-by: adester <Dsternejr@gmail.com>

---
+++ Assets/Scripts/GUI/MiniMapHUD.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class MiniMapHUD : MonoBehaviour 
+{
+    public Rect minimap = new Rect(Screen.width / 2 - 400, Screen.height / 2 - 400, 200, 200);
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+
+    public void OnGUI()
+    {
+        minimap = GUI.Window(0, minimap, MiniMapOverlay, "My Minimap");
+
+    }
+
+    public void MiniMapOverlay(int windowID)
+    {
+        
+
+
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
