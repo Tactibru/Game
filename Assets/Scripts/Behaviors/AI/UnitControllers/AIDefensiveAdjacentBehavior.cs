754f88e883cc2509d86fa3d8a5f86d6819b2c013
Hobo42 <taiteryan@gmail.com>
2013-11-17 16:05:52 -0700

    Created two AI scripts, AIDefensiveAdjacentBehavior.cs and AIOffensiveHunterBehavior.cs
    As of this commit, only AIDefensiveAdjacentBehavior.cs is complete BUT IS UNTESTED, while AIOffensiveHunterBehavior.cs is just Ken's AIOffensiveBehavior.cs copied over. I'm currently working on that one.
    
    Signed-off-by: Hobo42 <taiteryan@gmail.com>

---
+++ Assets/Scripts/Behaviors/AI/UnitControllers/AIDefensiveAdjacentBehavior.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// Author: Ryan Taite
+/// 
+/// Defines a defensive AI behavior. The squad will not move unless an enemy unit is
+/// in its movement area and there is a unit adjacent to the target unit.
+/// </summary>
+[AddComponentMenu("Tactibru/AI/Squad Behaviors/Defensive Adjacent Behavior")]
+public class AIDefensiveAdjacentBehavior : AIUnitBehavior
+{
+    /// <summary>
+    /// Determines the target for this squad.
+    /// </summary>
+    /// <returns>AI state the controller should enter after determining the target.</returns>
+    public override AIState DetermineTarget()
+    {
+        MovePointBehavior movePoint = Actor.currentMovePoint;
+        
+        // Get the maximum movement distance.
+        CombatSquadBehavior squadBehavior = Actor.GetComponent<CombatSquadBehavior>();
+
+        if (squadBehavior == null)
+        {
+            Debug.LogError(string.Format("AI Squad ({0}) does not have a CombatSquadBehavior!", Actor.transform.name));
+            Actor.actorHasMovedThisTurn = true;
+            
+            return AIState.PickingSquad;
+        }
+
+        int maxDistance = squadBehavior.Squad.Speed;
+        
+        // Retrieve a list of nodes within appropriate distance.
+        List<MovePointBehavior> graph = new List<MovePointBehavior>();
+        movePoint.BuildGraph(maxDistance, 0, grid, ref graph, true);
+        
+        Actor.actorHasMovedThisTurn = true;
+        
+        // Iterate over the nodes to determine if any node has an actor.
+        foreach (MovePointBehavior node in graph)
+        {
+            /// Node currently being checked
+            ActorBehavior actorOnNode = gameController.GetActorOnNode(node);
+
+            /// If the actor is null OR the actor is not on the player's team, find another node to check
+            if (actorOnNode == null || (actorOnNode.theSide != GameControllerBehaviour.UnitSide.player))
+            {
+                continue;
+            }
+                
+            /// Let's us see that we found an acceptable target
+            Debug.Log("Found target actor!");
+
+            /// Iterate over each node in the found target actor's neighborList for friendly units
+            foreach(MovePointBehavior neighborNode in actorOnNode.theGrid.targetNode.neighborList)
+            {
+                /// Node currently being checked
+                ActorBehavior neighborActorOnNode = gameController.GetActorOnNode(neighborNode);
+
+                /// If the actor is null OR the actor is not on the player's team, find another neighbor node to check
+                if (neighborActorOnNode == null || (neighborActorOnNode.theSide != GameControllerBehaviour.UnitSide.player))
+                {
+                    continue;
+                }
+
+                /// Let's us see that there was a unit adjacent to the target actor
+                Debug.Log("Found actor adjacent to target actor!");
+                
+                grid.ignoreList.Remove(node);
+                /// Create a path to the target actor
+                List<MovePointBehavior> path = movePoint.FindPath(node, maxDistance, grid);
+                path.RemoveAt(path.Count - 1);
+                
+                /// Attach path to our actor
+                Actor.pathList = path;
+                /// Actor can now move to his target
+                Actor.canMove = true;
+                
+                GridBehavior.preCombat = true;
+                grid.currentActor = Actor.gameObject;
+                grid.targetActor = actorOnNode.gameObject;
+                
+                grid.ignoreList.Add(node);
+                
+                return AIState.WaitingForCombat;
+            }
+        }
+        
+        return AIState.PickingSquad;
+    }
+}
---
+++ Assets/Scripts/Behaviors/AI/UnitControllers/AIOffensiveHunterBehavior.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Ryan Taite 
+/// 
+/// Defines an offensive AI behavior.
+/// The squad will select a target and move to attack it until it is dead or unavailable.
+/// </summary>
+[AddComponentMenu("Tactibru/AI/Squad Behaviors/Offensive Hunter Behavior")]
+public class AIOffensiveHunterBehavior : AIUnitBehavior
+{
+    /// <summary>
+    /// Determines the target for this squad.
+    /// </summary>
+    /// <returns>AI state the controller should enter after determining the target.</returns>
+    public override AIState DetermineTarget()
+    {
+        MovePointBehavior movePoint = Actor.currentMovePoint;
+        
+        // Get the maximum movement distance.
+        CombatSquadBehavior squadBehavior = Actor.GetComponent<CombatSquadBehavior>();
+
+        if (squadBehavior == null)
+        {
+            Debug.LogError(string.Format("AI Squad ({0}) does not have a CombatSquadBehavior!", Actor.transform.name));
+            Actor.actorHasMovedThisTurn = true;
+            
+            return AIState.PickingSquad;
+        }
+
+        int maxDistance = squadBehavior.Squad.Speed;
+        
+        // Retrieve a list of nodes within appropriate distance.
+        List<MovePointBehavior> graph = new List<MovePointBehavior>();
+        movePoint.BuildGraph(maxDistance, 0, grid, ref graph, true);
+        
+        Actor.actorHasMovedThisTurn = true;
+        
+        // Iterate over the nodes to determine if any node has an actor.
+        foreach (MovePointBehavior node in graph)
+        {
+            ActorBehavior actorOnNode = gameController.GetActorOnNode(node);
+            
+            if (actorOnNode == null || (actorOnNode.theSide != GameControllerBehaviour.UnitSide.player))
+                continue;
+            
+            grid.ignoreList.Remove(node);
+            List<MovePointBehavior> path = movePoint.FindPath(node, maxDistance, grid);
+            path.RemoveAt(path.Count - 1);
+            
+            Actor.pathList = path;
+            Actor.canMove = true;
+            
+            GridBehavior.preCombat = true;
+            grid.currentActor = Actor.gameObject;
+            grid.targetActor = actorOnNode.gameObject;
+            
+            grid.ignoreList.Add(node);
+            
+            return AIState.WaitingForCombat;
+        }
+        
+        int randomNode = Random.Range(0, graph.Count);
+        List<MovePointBehavior> squadPath = movePoint.FindPath(graph[randomNode], maxDistance, grid);
+        
+        Actor.pathList = squadPath;
+        Actor.canMove = true;
+        
+        return AIState.WaitingForMove;
+    }
+}
