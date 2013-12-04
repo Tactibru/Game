429eb3ad64703e472f7a3bab5757252a53993242
Ken 'Minalien' Murray <minalien@gmail.com>
2013-12-03 19:14:42 -0700

    Implemented "Fix Camera" utility in Tactibru menu.
    Signed-off-by: Ken 'Minalien' Murray <minalien@gmail.com>

--- Assets/Scripts/Editor/TactibruUtils.cs
+++ Assets/Scripts/Editor/TactibruUtils.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using UEd = UnityEditor.Editor;
 using UnityEditor;
 
@@ -10,7 +10,7 @@ namespace Tactibru.Editor
 		/// <summary>
 		/// Swaps the material on the selected object for the Unit material.
 		/// </summary>
-		[UnityEditor.MenuItem("Tactibru/Set Unit Material Properties")]
+		[MenuItem("Tactibru/Set Unit Material Properties")]
 		public static void SetUnitMaterialProperties()
 		{
 			Material unitMat = (Material)GameObject.Instantiate(Resources.Load ("Materials/UnitMaterial"));
@@ -26,6 +26,52 @@ namespace Tactibru.Editor
 			Selection.activeGameObject.renderer.sharedMaterial.SetColor ("_TargetHighlightColor", new Color((224.0f / 255.0f), (41.0f / 255.0f), (230.0f / 255.0f)));
 			Selection.activeGameObject.renderer.sharedMaterial.SetColor ("_TargetBaseColor", new Color((200.0f / 255.0f), (34.0f / 255.0f), (206.0f / 255.0f)));
 			Selection.activeGameObject.renderer.sharedMaterial.SetColor ("_TargetShadeColor", new Color((169.0f / 255.0f), (29.0f / 255.0f), (175.0f / 255.0f)));
+		}
+
+		/// <summary>
+		/// Swaps the selected camera for the appropriate camera in the asset database.
+		/// </summary>
+		[MenuItem("Tactibru/Fix Camera")]
+		public static void FixCamera()
+		{
+			GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+			// Destroy the camera in preparation for adding the new one.
+			GameObject.DestroyImmediate(mainCamera);
+
+			// Retrieve the MainCamera prefab from the asset database.
+			GameObject newCamera = (GameObject)GameObject.Instantiate(AssetDatabase.LoadAssetAtPath(
+				"Assets/Prefabs/Main Camera.prefab", typeof(GameObject)));
+
+			newCamera.transform.name = "Main Camera";
+
+			// Update the main camera property of the combat camera.
+			CombatSystemBehavior combatSystem = (CombatSystemBehavior)GameObject.Find("Combat Camera").GetComponent<CombatSystemBehavior>();
+			combatSystem.mainCamera = newCamera.camera;
+
+			EditorUtility.SetDirty (combatSystem);
+
+			// Disable all text renderers on the camera.
+			MeshRenderer[] renderers = newCamera.GetComponentsInChildren<MeshRenderer>();
+			foreach(MeshRenderer renderer in renderers)
+			{
+				if(renderer.gameObject.name == "MiniMapBG")
+					continue;
+
+				renderer.enabled = false;
+			}
+
+			// Update the grid for the minimap.
+			GridBehavior grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<GridBehavior>();
+			MiniMapGridBehaviour minimapGrid = newCamera.GetComponentInChildren<MiniMapGridBehaviour>();
+			minimapGrid.theGrid = grid;
+			minimapGrid.gameController = grid.GetComponent<GameControllerBehaviour>();
+
+			EditorUtility.SetDirty (newCamera);
+
+			Selection.activeGameObject = newCamera;
+
+			Debug.Log ("Camera fixed.");
 		}
 	}
 }
